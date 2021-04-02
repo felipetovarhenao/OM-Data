1b4395da4e27f8d0aeade652c8155160beac516f
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2021-04-02 13:36:35 -0500

    Fixed dtw-align bug and updated dtw-2 example

--- sources/OM-Data-functions.lisp
+++ sources/OM-Data-functions.lisp
@@ -675,7 +675,7 @@
     Example: 
     (dtw-align '(0 1 2 3 4 2) '(0 2 3 4 1) '((0 0) (1 0) (2 1) (3 2) (4 3) (5 4))) => ((0 0) (1 0) (2 2) (3 3) (4 4) (2 1))
     "
-    (if (and (eq (depth b) 1) (eq (depth posn) 2))
+    (if (eq (depth a) (depth b))
         (loop for p in posn collect
             (list (nth (first p) a) (nth (second p) b)))
         (mapcar #'(lambda (input1 input2) (DTW-align a input1 input2)) b posn)))
