bd4857247b41add0090e2c516c235305787fd749
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2021-06-16 15:29:59 -0500

    let* changes in misc.lisp

--- sources/misc.lisp
+++ sources/misc.lisp
@@ -9,15 +9,17 @@
 
 ; --------------- string-rewrite ---------------
 (defun string-rewrite (axiom rules iterations)
-    (setq axiom (flat (list axiom) 1))
-    (loop for n from 0 to (- iterations 1) do
-        (loop for a in axiom and i from 0 to (- (length axiom) 1) do
-            (loop for r in rules do
-                (if (equal a (first r))
-                    (progn
-                        (setf (nth i axiom) (second r))))))
-        (setq axiom (flat axiom 1)))
-    axiom)
+    (let*
+        (
+            (axiom (flat (list axiom) 1)))
+        (loop for n from 0 to (- iterations 1) do
+            (loop for a in axiom and i from 0 to (- (length axiom) 1) do
+                (loop for r in rules do
+                    (if (equal a (first r))
+                        (progn
+                            (setf (nth i axiom) (second r))))))
+            (setf axiom (flat axiom 1)))
+        axiom))
 
 ; --------------- L-system ---------------
 (defmethod! L-system ((axiom number) (rules list) (generations integer))
@@ -39,38 +41,41 @@
     :indoc '("list" "list" "list" "list" "number")
     :icon 000
     :doc "2D Turtle graphics"
-    (setq x 0)
-    (setq y 0)
-    (setq mag 0)
-    (setq memory nil)
-    (setq out (list (om-make-point x y)))
-    (loop for s in lsys do
-        (loop for mr in mag-rules do
-            (if (equal s (first mr))
-                (progn
-                    (setq mag (second mr))
-                    (setq x (+ x (* mag (cos (deg->rad theta)))))
-                    (setq y (+ y (* mag (sin (deg->rad theta)))))
-                    (setq out (append out (list (om-make-point x y)))))))
-        (loop for tr in theta-rules do
-            (if (equal s (first tr))
-                (progn
-                    (setq theta (+ theta (second tr))))))
-        (loop for mr in memory-rules do
-            (if (equal s (first mr))
-                (progn 
-                    (if (equal (second mr) 1)
-                        (setq memory (append memory (list (list x y theta mag))))
-                    )
-                    (if (equal (second mr) 0)
-                        (progn
-                            (setq state (car (last memory)))
-                            (setq x (first state))
-                            (setq y (second state))
-                            (setq theta (third state))
-                            (setq mag (fourth state))
-                            (setq memory (butlast memory))))))))
-    (make-instance 'bpc :point-list out))
+    (let*
+        (
+            (x 0)
+            (y 0)
+            (mag 0)
+            (memory nil)
+            (out (list (om-make-point x y)))
+            (state nil))
+        (loop for s in lsys do
+            (loop for mr in mag-rules do
+                (if (equal s (first mr))
+                    (progn
+                        (setf mag (second mr))
+                        (setf x (+ x (* mag (cos (deg->rad theta)))))
+                        (setf y (+ y (* mag (sin (deg->rad theta)))))
+                        (setf out (append out (list (om-make-point x y)))))))
+            (loop for tr in theta-rules do
+                (if (equal s (first tr))
+                    (progn
+                        (setf theta (+ theta (second tr))))))
+            (loop for mr in memory-rules do
+                (if (equal s (first mr))
+                    (progn 
+                        (if (equal (second mr) 1)
+                            (setf memory (append memory (list (list x y theta mag))))
+                        )
+                        (if (equal (second mr) 0)
+                            (progn
+                                (setf state (car (last memory)))
+                                (setf x (first state))
+                                (setf y (second state))
+                                (setf theta (third state))
+                                (setf mag (fourth state))
+                                (setf memory (butlast memory))))))))
+        (make-instance 'bpc :point-list out)))
 
 ;--------------- Make-sieve ---------------
 (defmethod! Make-sieve ((list list) (reps integer) sieve-mode sieve-type &optional (offset '0))
@@ -81,50 +86,64 @@
         (3 (("nil" 'nil) ("complement" 'complement))))
     :icon 000
     :doc "Builds N full periods of a sieve, based on a list of integers. Make-sieve is meant to be a compact version of OM's native CRIBLE class and functions"
-    (setq list (remove 0 list))
-    (setq period (+ offset (* reps (list-lcm list))))
-    (setq sieves (loop for l in list collect
-        (arithm-ser offset period l)))
-    (cond
-        (
-            (equal sieve-mode 'union)
-            (setq out (list-union sieves)))
-        (
-            (equal sieve-mode 'diff)
-            (setq out (list-diff sieves))))
-    (if (equal sieve-type 'complement)
-        (setq out (list-diff (list (arithm-ser offset period 1) (flat (list out))))))
-    (stable-sort out #'<))
+    (let* ()
+        (setf list (remove 0 list))
+        (let*
+            (
+                (period (+ offset (* reps (list-lcm list))))
+                (sieves (loop for l in list collect
+                    (arithm-ser offset period l)))
+            )
+            (cond
+                (
+                    (equal sieve-mode 'union)
+                    (setf out (list-union sieves)))
+                (
+                    (equal sieve-mode 'diff)
+                    (setf out (list-diff sieves))))
+            (if (equal sieve-type 'complement)
+                (setf out (list-diff (list (arithm-ser offset period 1) (flat (list out))))))
+            (stable-sort out #'<))))
 
 (defun list-union (list)
-    (setq out (car list))
-    (loop for l in (cdr list) do
-        (setq out (x-union out l)))
-    out)
+    (let*
+        (
+            (out (car list)))
+        (loop for l in (cdr list) do
+            (setf out (x-union out l)))
+        out))
 
 (defun list-intersect (list)
-    (setq out (car list))
-    (loop for l in (cdr list) do
-        (setq out (x-intersect out l)))
-    out)
+    (let*
+        (
+            (out (car list)))
+        (loop for l in (cdr list) do
+            (setf out (x-intersect out l)))
+        out))
 
 (defun list-diff (list)
-    (setq out (car list))
-    (loop for l in (cdr list) do
-        (setq out (x-diff out l)))
-    out)
+    (let*
+        (
+            (out (car list)))
+        (loop for l in (cdr list) do
+            (setf out (x-diff out l)))
+        out))
 
 (defun list-lcm (list)
-    (setq out (car list))
-    (loop for l in (cdr list) do
-        (setq out (lcm out l)))
-    out)
+    (let*
+        (
+            (out (car list)))
+        (loop for l in (cdr list) do
+            (setf out (lcm out l)))
+        out))
 
 (defun list-gcd (list)
-    (setq out (car list))
-    (loop for l in (cdr list) do
-        (setq out (gcd out l)))
-    out)
+    (let*
+        (
+            (out (car list)))
+        (loop for l in (cdr list) do
+            (setf out (gcd out l)))
+        out))
 
 ;--------------- Vieru-sequence ---------------
 (defmethod! Vieru-seq ((seq list) (n-tiers integer))
@@ -137,21 +156,28 @@
     (vieru-seq '(4 1 2 1 4) 1) => ((9 1 11 3 0))
     (vieru-seq '(2 1 4 1 2) 3) => ((9 3 7 1 0) (4 4 4 9 9) (0 0 5 0 5))
     "
-    (setq mod-n (reduce #'+ seq))
-    (setq seq (nth-value 1 (om// (append seq (list (car seq))) mod-n)))
-    (setq out nil)
-    (loop for x from 1 to n-tiers do
-        (setq diff-seq nil)
-        (loop for i from 0 to (- (length seq) 2) do
-            (setq current (nth i seq))
-            (setq next (nth (+ i 1) seq))
-            (if (>= next current)
-                (setq val (abs (- next current)))
-                (setq val (abs (- (+ next mod-n) current))))
-            (setq diff-seq (append diff-seq (list val))))
-        (setq out (append out (list diff-seq)))
-        (setq seq (append diff-seq (list (car diff-seq)))))
-    out)
+    (let*   
+        (
+            (out nil)
+            (mod-n (reduce #'+ seq)))
+        (setf seq (nth-value 1 (om// (append seq (list (car seq))) mod-n)))
+        (loop for x from 1 to n-tiers do
+            (let*
+                (
+                    (diff-seq nil))
+                (loop for i from 0 to (- (length seq) 2) do
+                    (let*
+                        (
+                            (current (nth i seq))
+                            (next (nth (+ i 1) seq))
+                            (val nil))
+                        (if (>= next current)
+                            (setf val (abs (- next current)))
+                            (setf val (abs (- (+ next mod-n) current))))
+                        (setf diff-seq (append diff-seq (list val)))))
+                (setf out (append out (list diff-seq)))
+                (setf seq (append diff-seq (list (car diff-seq))))))
+        out))
 
 ;--------------- Recursivus ---------------
 (defmethod! Recursivus ((list-a list) (list-b list) &optional (rotations nil))
@@ -161,24 +187,30 @@
     :icon 000
     :doc "Given two numeric lists A and B, outputs a list of possible extensions of A through segments B. More precisely, it searches for segments of B, where the sum of changes in those segments match the change (i.e. interval) between adjacent elements in A. Based on the technique described by Antonio de Sousa Dias in 'Free transposition of Audio Processing Techniques into the Domain of Notes'
     "
-    (setq dx-a (x->dx (append list-a (last list-a))))
-    (setq numrot (length list-b))
-    (setq out (mat-trans (list (mat-trans (list list-a)))))
-    (if (equal rotations nil)
-        (setq forms (list (x->dx list-b)))
-        (setq forms 
-            (loop for r from 0 to (- numrot 1) collect
-                (x->dx (rotate list-b r)))))
-    (loop for dx-b in forms do
-        (loop for fn from 1 to numrot do
-            (setq b-seg (first-n dx-b fn))
-            (setq dx-seg (reduce #'+ b-seg))
-            (loop for a in dx-a and x from 0 to (length dx-a) do
-                (if (equal dx-seg a)
-                    (progn
-                        (setq mel-seg (dx->x (nth x list-a) b-seg))
-                        (if (> (length mel-seg) 2)
-                            (setq mel-seg (butlast mel-seg)))
-                        (if (equal nil (position mel-seg (nth x out) :test 'equal))
-                            (setq out (subs-posn out x (append (nth x out) (list mel-seg))))))))))
-    out)
+    (let*
+        (
+            (dx-a (x->dx (append list-a (last list-a))))
+            (numrot (length list-b))
+            (out (mat-trans (list (mat-trans (list list-a)))))
+            (forms nil))
+        (if (equal rotations nil)
+            (setf forms (list (x->dx list-b)))
+            (setf forms 
+                (loop for r from 0 to (- numrot 1) collect
+                    (x->dx (rotate list-b r)))))
+        (loop for dx-b in forms do
+            (loop for fn from 1 to numrot do
+                (let*
+                    (
+                        (b-seg (first-n dx-b fn))
+                        (dx-seg (reduce #'+ b-seg))
+                        (mel-seg nil))
+                    (loop for a in dx-a and x from 0 to (length dx-a) do
+                        (if (equal dx-seg a)
+                            (progn
+                                (setf mel-seg (dx->x (nth x list-a) b-seg))
+                                (if (> (length mel-seg) 2)
+                                    (setf mel-seg (butlast mel-seg)))
+                                (if (equal nil (position mel-seg (nth x out) :test 'equal))
+                                    (setf out (subs-posn out x (append (nth x out) (list mel-seg)))))))))))
+        out))
--- sources/rhythm.lisp
+++ sources/rhythm.lisp
@@ -14,24 +14,26 @@
     :icon 000
     :doc "euclidean-rhythm, as the name suggests, computes the Euclidean rhythm for a given period k and number of beats/pulses n, where n < k. The output can be interpreted as a list of intervals or distances between between pulses
     "
-    (setq a numbeats)
-    (setq b (- period numbeats))
-    (setq binary-seq (list (repeat-n 1 a) (repeat-n 0 b)))
-    (while (> (min a b) 1)
-        (setq binary-seq (mapcar #'(lambda (input) (remove nil (flat input))) (mat-trans binary-seq)))
-        (setq binary-seq (group-list binary-seq (sort-list (list a b)) 'linear))
-        (setq a (length (car binary-seq)))
-        (setq b (length (second binary-seq))))
-    (setq binary-seq (flat binary-seq))
-    (setq val 1)
-    (setq out nil)
-    (loop for n in binary-seq do
-        (if (eq n 0)
-            (setq val (+ val 1))
-            (progn
-                (setq out (append out (list val)))
-                (setq val 1))))
-    (rotate (cdr (append out (list val))) rotation))
+    (let*   
+        (
+            (a numbeats)
+            (b (- period numbeats))
+            (binary-seq (list (repeat-n 1 a) (repeat-n 0 b)))
+            (val 1)
+            (out nil))
+        (while (> (min a b) 1)
+            (setf binary-seq (mapcar #'(lambda (input) (remove nil (flat input))) (mat-trans binary-seq)))
+            (setf binary-seq (group-list binary-seq (sort-list (list a b)) 'linear))
+            (setf a (length (car binary-seq)))
+            (setf b (length (second binary-seq))))
+        (setf binary-seq (flat binary-seq))
+        (loop for n in binary-seq do
+            (if (eq n 0)
+                (setf val (+ val 1))
+                (progn
+                    (setf out (append out (list val)))
+                    (setf val 1))))
+        (rotate (cdr (append out (list val))) rotation)))
 
 ;--------------- Rhythmicon ---------------
 (defmethod! Rhythmicon ((base-dur number) (subdivisions list) (times integer) mode)
@@ -41,79 +43,98 @@
     :menuins '((3 (("div" 'div) ("mult" 'mult))))
     :doc "Outputs a rhythmicon as a MULTI-SEQ, given a fundamental duration (ms), a list of subdivisions, and a number of repetitions. For each rhythmic partial, the corresponding pitch is automatically assigned, using 3300 as the fundamental.
     "
-    (setq onsets nil)
-    (setq pitches nil)
-    (setq durations nil)
-    (setq vels (om-scale subdivisions 120 30))
-    (setq f0 (mc->f 3300))
-    (setq output nil)
-    (setq velocities nil)
-    (setq max-onset (* base-dur times))
-    (if (equal mode 'mult)
-        (setq period (list-lcm subdivisions))
-    )
-    (loop for m in subdivisions and v in vels do
-        (cond
-            (
-                (equal mode 'div)
-                (progn
-                    (setq numnotes (nth-value 0 (ceiling (* m times))))
-                    (setq durs (repeat-n (/ base-dur m) numnotes))
-                    (setq pre-onsets (om-clip (dx->x 0 durs) 0 max-onset))
-                    (setq onsets (append onsets (list pre-onsets)))
-                    (setq durations (append durations (list (x->dx pre-onsets))))
-                    (setq pitches (append pitches (list (repeat-n (f->mc (* f0 m)) numnotes))))
-                    (setq velocities (append velocities (list (repeat-n v numnotes))))))
-            (
-                (equal mode 'mult)
-                (progn
-                    (setq numnotes (* (/ period m) times))
-                    (setq durs (repeat-n (* base-dur m) numnotes))
-                    (setq pre-onsets (dx->x 0 durs))
-                    (setq onsets (append onsets (list pre-onsets)))
-                    (setq durations (append durations (list (x->dx pre-onsets))))
-                    (setq pitches (append pitches (list (repeat-n (f->mc (* f0 m)) numnotes))))
-                    (setq velocities (append velocities (list (repeat-n v numnotes))))))))
-    (make-instance 'multi-seq :chord-seqs (reverse (loop for o in onsets and p in pitches and d in durations and v in velocities collect 
-        (make-instance 'chord-seq 
-            :lmidic p 
-            :lonset o 
-            :ldur d 
-            :lvel v)))))
+    (let*
+        (
+            (onsets nil)
+            (pitches nil)
+            (durations nil)
+            (vels (om-scale subdivisions 120 30))
+            (f0 (mc->f 3300))
+            (output nil)
+            (velocities nil)
+            (max-onset (* base-dur times))
+            (period nil)
+            (pitches nil))
+        (if (equal mode 'mult)
+            (progn 
+                (setf subdivisions (om-round subdivisions))
+                (setf period (list-lcm subdivisions))))
+        (loop for m in subdivisions and v in vels do
+            (let*
+                (
+                    (numnotes nil)
+                    (durs nil)
+                    (pre-onsets nil)
+                )
+                (cond
+                    (
+                        (equal mode 'div)
+                        (progn
+                            (setf numnotes (nth-value 0 (ceiling (* m times))))
+                            (setf durs (repeat-n (/ base-dur m) numnotes))
+                            (setf pre-onsets (om-clip (dx->x 0 durs) 0 max-onset))
+                            (setf onsets (append onsets (list pre-onsets)))
+                            (setf durations (append durations (list (x->dx pre-onsets))))
+                            (setf pitches (append pitches (list (repeat-n (f->mc (* f0 m)) numnotes))))
+                            (setf velocities (append velocities (list (repeat-n v numnotes))))))
+                    (
+                        (equal mode 'mult)
+                        (progn
+                            (setf numnotes (* (/ period m) times))
+                            (setf durs (repeat-n (* base-dur m) numnotes))
+                            (setf pre-onsets (dx->x 0 durs))
+                            (setf onsets (append onsets (list pre-onsets)))
+                            (setf durations (append durations (list (x->dx pre-onsets))))
+                            (setf pitches (append pitches (list (repeat-n (f->mc (* f0 m)) numnotes))))
+                            (setf velocities (append velocities (list (repeat-n v numnotes)))))))))
+        (make-instance 'multi-seq :chord-seqs (reverse (loop for o in onsets and p in pitches and d in durations and v in velocities collect 
+            (make-instance 'chord-seq 
+                :lmidic p 
+                :lonset o 
+                :ldur d 
+                :lvel v))))))
 
 ; --------------- Risset rhythm---------------
 (defun riss-onsets (onsets totdur period v mode)
-    (setq totdur (list-max onsets))
-    (setq v-exp (expt 2 v))
-    (setq tl nil)
-    (loop for x from 0 to (- v-exp 1) do
-        (setq tl (append tl (om+ (butlast onsets) (om* totdur x)))))
-    (setq tl (om/ (append tl (list (* v-exp totdur))) (* v-exp totdur)))
-    (cond 
+    (let*
         (
-            (equal mode 0)
-            (setq te (om-log (om+ 1 tl) 2)))
-        (
-            (equal mode 1)
-            (setq te (om- (om^ 2 tl) 1))))
-    (om* te period))
+            (totdur (list-max onsets))
+            (v-exp (expt 2 v))
+            (tl nil)
+            (te))
+        (loop for x from 0 to (- v-exp 1) do
+            (setf tl (append tl (om+ (butlast onsets) (om* totdur x)))))
+        (setf tl (om/ (append tl (list (* v-exp totdur))) (* v-exp totdur)))
+        (cond 
+            (
+                (equal mode 0)
+                (setf te (om-log (om+ 1 tl) 2)))
+            (
+                (equal mode 1)
+                (setf te (om- (om^ 2 tl) 1))))
+        (om* te period)))
 
 (defun riss-rates (te totdur period v mode)
-    (setq normalized (om/ te period))
-    (if (equal mode 0)
-        (setq normalized (om^ 2 (om+ v normalized)))
-        (progn 
-            (setq minval (expt 2 v))
-            (setq maxval (expt 2 (+ v 1)))
-            (setq normalized (om-scale (om* (om+ (om-log (om+ normalized 1) 2) 1) minval) maxval minval))))
-    (setq rates (om* (/ (* totdur (log 2)) period) normalized))
-    rates)
+    (let*
+        (
+            (normalized (om/ te period))
+            (minval nil)
+            (maxval nil))
+        (if (equal mode 0)
+            (setf normalized (om^ 2 (om+ v normalized)))
+            (progn 
+                (setf minval (expt 2 v))
+                (setf maxval (expt 2 (+ v 1)))
+                (setf normalized (om-scale (om* (om+ (om-log (om+ normalized 1) 2) 1) minval) maxval minval))))
+        (om* (/ (* totdur (log 2)) period) normalized)))
 
 (defun riss-amps (te period voices v mode)
-    (setq index-list (om/ te period))
-    (if (equal mode 1)
-        (setq index-list (om- 1 index-list)))
-    (setq amp (om+ 0.5 (om* -0.5 (om-cos (om* (* pi 2) (om+ (/ v voices) (om/ index-list voices))))))))
+    (let* 
+        (
+            (index-list (om/ te period)))
+        (if (equal mode 1)
+            (setf index-list (om- 1 index-list)))
+        (om+ 0.5 (om* -0.5 (om-cos (om* (* pi 2) (om+ (/ v voices) (om/ index-list voices))))))))
 
 ;--------------- Risset-rhyhtm ---------------
 (defmethod! Risset-rhythm ((self chord-seq) (speed number) (voices integer) (rep integer) &optional (onset-mode '0) (mc-mode '2))
@@ -122,50 +143,56 @@
     :icon 000
     :menuins '((4 (("accelerando" '0) ("ritardando" '1))) (5 (("preserve pitch" '0) ("period-wise transp." '1) ("chord-wise transp." '2))))
     :doc "Builds a seemingly eternal accelerando/ritardando, based on an input CHORD-SEQ, a speed factor, number of voices, number of repetitions."
-
-    (setq mc-list (lmidic self))
-    (setq onsets (lonset self))
-    (setq durs (ldur self))
-    (setq vels (lvel self))
-    (setq offsets (loffset self))
-    (setq chans (lchan self))
-    (setq leg (legato self))
-
-    (setq out nil)
-    (setq totdur (list-max onsets))
-    (setq period (* totdur speed))
-    (setq fq-list (mc->f mc-list))
-
-    (loop for v from 0 to (- voices 1) do
-        (setq v-exp (expt 2 v))
-        (setq new-onsets (riss-onsets onsets totdur period v onset-mode))
-        (setq rates (riss-rates new-onsets totdur period v onset-mode))
-        (setq amps (riss-amps new-onsets period voices v onset-mode))
-        (setq new-mc nil)
-        (setq new-vels nil)
-        (setq new-durs nil)
-        (loop for f in (flat (repeat-n fq-list v-exp) 1) and v in (flat (repeat-n vels v-exp) 1) and d in (flat (repeat-n durs v-exp) 1) and a in (butlast amps) and r in (butlast rates) and n from 0 to (- (length rates) 1) do
-            (setq new-durs (append new-durs (list (om/ d r))))
-            (if (equal mc-mode 1)
-                (progn
-                    (setq r-index (* (nth-value 0 (floor (/ n (length durs)))) (length durs)))
-                    (setq r (nth r-index rates))))
-            (if (> mc-mode 0)
-                (setq f (om* r f)))
-            (setq new-mc (append new-mc (list (f->mc f))))
-            (setq new-vels (append new-vels (list (om* v a)))))
-        (setq te (flat 
-            (loop for x from 0 to (- rep 1) collect
-                (om+ (butlast new-onsets) (* x period))
-            ) 1))
-        (setq te (append te (list (* rep (list-max new-onsets)))))
-        (setq seq (make-instance 'chord-seq 
-            :lmidic (flat (repeat-n new-mc rep) 1) 
-            :lonset te 
-            :ldur (flat (repeat-n new-durs rep) 1)  
-            :lvel (flat (repeat-n new-vels rep) 1)
-            :loffset (flat (repeat-n offsets (* rep v-exp)) 1)
-            :lchan (flat (repeat-n chans (* rep v-exp)) 1)
-            :legato leg))
-        (setq out (append out (list seq))))
-    (reverse out))
+    (let*
+        (
+            (out nil)
+            (mc-list (lmidic self))
+            (onsets (lonset self))
+            (durs (ldur self))
+            (vels (lvel self))
+            (offsets (loffset self))
+            (chans (lchan self))
+            (leg (legato self))
+            (totdur (list-max onsets))
+            (period (* totdur speed))
+            (fq-list (mc->f mc-list)))
+        (loop for v from 0 to (- voices 1) do
+            (let*
+                (
+                    (v-exp (expt 2 v))
+                    (new-onsets (riss-onsets onsets totdur period v onset-mode))
+                    (rates (riss-rates new-onsets totdur period v onset-mode))
+                    (amps (riss-amps new-onsets period voices v onset-mode))
+                    (new-mc nil)
+                    (new-vels nil)
+                    (new-durs nil)
+                    (te nil)
+                    (seq nil))
+                (loop for f in (flat (repeat-n fq-list v-exp) 1) and v in (flat (repeat-n vels v-exp) 1) and d in (flat (repeat-n durs v-exp) 1) and a in (butlast amps) and r in (butlast rates) and n from 0 to (- (length rates) 1) do
+                    (let*
+                        (
+                            (r-index nil))
+                        (setf new-durs (append new-durs (list (om/ d r))))
+                        (if (equal mc-mode 1)
+                            (progn
+                                (setf r-index (* (nth-value 0 (floor (/ n (length durs)))) (length durs)))
+                                (setf r (nth r-index rates))))
+                        (if (> mc-mode 0)
+                            (setf f (om* r f)))
+                        (setf new-mc (append new-mc (list (f->mc f))))
+                        (setf new-vels (append new-vels (list (om* v a))))))
+                (setf te (flat 
+                    (loop for x from 0 to (- rep 1) collect
+                        (om+ (butlast new-onsets) (* x period))
+                    ) 1))
+                (setf te (append te (list (* rep (list-max new-onsets)))))
+                (setf seq (make-instance 'chord-seq 
+                    :lmidic (flat (repeat-n new-mc rep) 1) 
+                    :lonset te 
+                    :ldur (flat (repeat-n new-durs rep) 1)  
+                    :lvel (flat (repeat-n new-vels rep) 1)
+                    :loffset (flat (repeat-n offsets (* rep v-exp)) 1)
+                    :lchan (flat (repeat-n chans (* rep v-exp)) 1)
+                    :legato leg))
+                (setf out (append out (list seq)))))
+        (reverse out)))
